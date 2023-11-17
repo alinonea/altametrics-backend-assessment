218fffa950aa88432b8685f6ba23f5585211c977
Alin Onea <oneaalinx@gmail.com>
2023-11-17 04:07:38 +0200

    Removed unused content

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { InvoiceModule } from './invoice/invoice.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, InvoiceModule, PrismaModule, ConfigModule.forRoot({isGlobal: true})],
+  imports: [AuthModule, InvoiceModule, PrismaModule, ConfigModule.forRoot({isGlobal: true})],
 })
 export class AppModule {}
